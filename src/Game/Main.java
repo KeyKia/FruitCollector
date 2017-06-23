e6c49be7e3ab3e538a76bc840bd6fed4f6f51774
TonyS <Jamadi1377@gmail.com>
2017-06-23 17:39:06 +0430

    Initial commit

---
+++ src/Game/Main.java
@@ -0,0 +1,22 @@
+package Game;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Group root = new Group();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
