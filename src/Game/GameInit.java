766117f65fae64d114982b68fc2c9949cb2df19b
TonyS <Jamadi1377@gmail.com>
2017-06-23 18:20:06 +0430

    First push of Game menu

--- src/Game/GameInit.java
+++ src/Game/GameInit.java
@@ -4,6 +4,9 @@ package Game;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 
 /**
  * Created by Ali Jamadi on 6/23/17.
@@ -14,8 +17,18 @@ class GameInit {
     private Scene scene = new Scene(root);
 
     GameInit() {
-        Canvas background = new Canvas(300,700);
+        Canvas background = new Canvas(500,700);
+        Image back = new Image("file:Resources/JungleBack.png");
+        background.getGraphicsContext2D().drawImage(back,0,0,500,700);
+        Button startGameBtn = new Button("Start");
+        startGameBtn.setLayoutX(background.getWidth()/2-20);
+        startGameBtn.setLayoutY(background.getHeight()/2+20);
+
+
+
         root.getChildren().add(background);
+        root.getChildren().addAll(startGameBtn);
+
     }
 
     Scene getScene() {
--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,8 +1,6 @@
 package Game;
 
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,7 +9,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         GameInit game = new GameInit();
 
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Fruit Collector");
         primaryStage.setScene(game.getScene());
         primaryStage.show();
     }
