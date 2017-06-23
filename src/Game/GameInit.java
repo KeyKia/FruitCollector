e95f4f1bbe1c26218b38a78332c68865e6838005
TonyS <jamadi1377@gmail.com>
2017-06-23 18:54:59 +0430

    Development Try

--- src/Game/GameInit.java
+++ src/Game/GameInit.java
@@ -5,8 +5,8 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 
 /**
  * Created by Ali Jamadi on 6/23/17.
@@ -20,14 +20,30 @@ class GameInit {
         Canvas background = new Canvas(500,700);
         Image back = new Image("file:Resources/JungleBack.png");
         background.getGraphicsContext2D().drawImage(back,0,0,500,700);
-        Button startGameBtn = new Button("Start");
-        startGameBtn.setLayoutX(background.getWidth()/2-20);
-        startGameBtn.setLayoutY(background.getHeight()/2+20);
+
+        Button singlePlayerBtn = new Button("Single Player");
+        singlePlayerBtn.setLayoutX(background.getWidth()/2-110);
+        singlePlayerBtn.setLayoutY(background.getHeight()/2+20);
+
+        Button multiPlayerBtn = new Button("Multi Player");
+        multiPlayerBtn.setLayoutX(singlePlayerBtn.getLayoutX()+150 );
+        multiPlayerBtn.setLayoutY(background.getHeight()/2+20);
+
+//        CheckBox music = new CheckBox("Music");
+//        music.setSelected(true);
+//        music.setLayoutY(multiPlayerBtn.getLayoutY()+20);
+//        music.setLayoutX(singlePlayerBtn.getLayoutX());
+//
+//        CheckBox soundEffect = new CheckBox("Sound Effect");
+//        soundEffect.setSelected(true);
+//        soundEffect.setLayoutY(multiPlayerBtn.getLayoutY()+20);
+//        soundEffect.setLayoutX(singlePlayerBtn.getLayoutX());
+
 
 
 
         root.getChildren().add(background);
-        root.getChildren().addAll(startGameBtn);
+        root.getChildren().addAll(singlePlayerBtn,multiPlayerBtn);
 
     }
 
