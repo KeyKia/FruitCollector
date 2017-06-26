1f184f4bd66905c39e75b754647ea6fa241cd395
TonyS <Jamadi1377@gmail.com>
2017-06-26 14:49:49 +0430

    minor Graphics improvement + orange falling with random angels

--- src/Objects/Fruits/Orange.java
+++ src/Objects/Fruits/Orange.java
@@ -12,20 +12,25 @@ import javafx.scene.image.Image;
 public class Orange extends Fruits {
     static private final Image img = new Image("file:Resources/images/Orange.png");
     private boolean left;
+    private int angel;
+    private double horizantalSpeed;
     public Orange() {
         super(((int) (GameScene.UNIT * 10)), ( (40 * GameScene.UNIT / GameScene.SPEED_CONVERTER)), 40, img);
         if (position > 10 && position < 90)
             left = random.nextBoolean();
         else left = position > 10;
+        angel = random.nextInt(60) + 30;
+        double a = speed / Math.sin(angel);
+        horizantalSpeed = a * Math.cos(angel);
     }
 
     @Override
     public void move(Canvas fruitCanvas, double width, double start) {
         if (left) {
-            fruitCanvas.setLayoutX(fruitCanvas.getLayoutX() - speed / 2.00);
+            fruitCanvas.setLayoutX(fruitCanvas.getLayoutX() - horizantalSpeed);
             fruitCanvas.setLayoutY(fruitCanvas.getLayoutY() + speed);
         } else {
-            fruitCanvas.setLayoutX(fruitCanvas.getLayoutX() + speed / 2.00);
+            fruitCanvas.setLayoutX(fruitCanvas.getLayoutX() + horizantalSpeed);
             fruitCanvas.setLayoutY(fruitCanvas.getLayoutY() + speed);
         }
     }
