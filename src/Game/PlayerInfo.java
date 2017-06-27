e700ed8a79f8e9243c8d0bf4e3cc0cb80a57f3d1
jeem <arash.jeem@gmail.com>
2017-06-27 20:45:31 +0430

    small reformat in resources images structure 10 avatar images added  avatar with random avatar image executed  worms images were moved and changed in class

--- src/Game/PlayerInfo.java
+++ src/Game/PlayerInfo.java
@@ -2,6 +2,8 @@ package Game;
 
 import javafx.scene.image.Image;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by Ali Jamadi on 6/24/17.
  * 9512762630
@@ -15,6 +17,8 @@ public class PlayerInfo {
     public PlayerInfo(String nickName) {
         this.nickName = nickName;
         //TODO:Set avatar;
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 10 + 1); //min, max+1
+        avatar = new Image("file:Resources/images/avatars/"+randomNum+".png");
     }
 
     public String getNickName() {
--- src/Objects/Fruits/WormHalfer.java
+++ src/Objects/Fruits/WormHalfer.java
@@ -8,7 +8,7 @@ import javafx.scene.image.Image;
  * Created by jeem on 6/27/2017.
  */
 public class WormHalfer extends Fruits {
-    static private final Image img = new Image("file:Resources/images/wormHalfer.png");
+    static private final Image img = new Image("file:Resources/images/worms/wormHalfer.png");
 
     public WormHalfer() {
         super(((int) (GameScene.UNIT * 30)), ( (100 * GameScene.UNIT / GameScene.SPEED_CONVERTER)), 0, img);
